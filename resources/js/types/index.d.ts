d6309d4b26d5cbe3e3b4ce06f95dada25db9528e
Ali Sultani <aliakbar.su@hotmail.com>
2024-02-24 19:57:05 +1300

    Refactor types

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,15 +1,96 @@
 import { Config } from 'ziggy-js';
 
-export interface User {
-    id: number;
-    name: string;
-    email: string;
-    email_verified_at: string;
+export enum listingTypeEnum {
+  rent = 'rent',
+  sell = 'sell',
 }
 
-export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
-    auth: {
-        user: User;
-    };
-    ziggy: Config & { location: string };
+export enum ListingStatusEnums {
+  wishlist = 'wishlist',
+  viewed = 'viewed',
+  viewing = 'viewing',
+  applied = 'applied',
+  offer_accepted = 'offer_accepted',
+  offer_declined = 'offer_declined',
+}
+
+export enum RentFrequencyEnums {
+  weekly = 'weekly',
+  forthnightly = 'forthnightly',
+  monthly = 'monthly',
+}
+
+export interface Note {
+  id: string;
+  listing_id: string;
+  note: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Board {
+  id: string;
+  user_id: string;
+  name: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Image {
+  id: string;
+  listing_id: string;
+  url: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Listing {
+  id: string;
+  address: string;
+  title: string;
+  description: string;
+  type: listingTypeEnum;
+  property_type: string;
+  rent: number;
+  rent_frequency: string;
+  bedrooms: number;
+  bathrooms: number;
+  toilets: number;
+  garages: number;
+  status: ListingStatusEnums;
+  board_id: string;
+  notes: Note[];
+  board: Board;
+  images: Image[];
+  created_at: string;
+  updated_at: string;
+}
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  boards: Board[];
+  listings: Listing[];
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Plan {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  features: string[];
+  created_at: string;
+  updated_at: string;
+}
+
+export type PageProps<
+  T extends Record<string, unknown> = Record<string, unknown>,
+> = T & {
+  auth: {
+    user: User;
+  };
+  ziggy: Config & { location: string };
 };
