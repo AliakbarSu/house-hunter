8711a5a1e840d01547007955cf2539e220918f61
Ali Sultani <aliakbar.su@hotmail.com>
2024-02-24 13:35:44 +1300

    Add frontend type interfaces

---
+++ resources/js/types/application.d.ts
@@ -0,0 +1,66 @@
+export enum listingTypeEnum {
+  Rent = 'rent',
+  sell = 'sell',
+}
+
+export enum ListingStatusEnums {
+  wishlist = 'wishlist',
+  viewed = 'viewed',
+  viewing = 'viewing',
+  applied = 'applied',
+  offer_accepted = 'offer_accepted',
+  offer_declined = 'offer_declined',
+}
+
+export enum RentFrequencyEnums {
+  weekly = 'weekly',
+  forthnightly = 'forthnightly',
+  monthly = 'monthly',
+}
+
+export interface Note {
+  id: string;
+  listing_id: string;
+  note: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Board {
+  id: string;
+  user_id: string;
+  name: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Listing {
+  id: string;
+  address: string;
+  title: string;
+  description: string;
+  type: listingTypeEnum;
+  property_type: string;
+  rent: number;
+  rent_frequency: string;
+  bedrooms: number;
+  bathrooms: number;
+  toilets: number;
+  garages: number;
+  status: ListingStatusEnums;
+  board_id: string;
+  notes: Note[];
+  board: Board;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  boards: Board[];
+  listings: Listing[];
+  created_at: string;
+  updated_at: string;
+}
