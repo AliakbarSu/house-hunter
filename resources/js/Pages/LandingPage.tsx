b2f77f5d19006abfd4dc1b4475bbb7415707d673
Ali Sultani <aliakbar.su@hotmail.com>
2024-02-28 22:59:48 +1300

    Add page title

--- resources/js/Pages/LandingPage.tsx
+++ resources/js/Pages/LandingPage.tsx
@@ -2,6 +2,7 @@ import Pricing from '@/Components/LandingPage/Pricing';
 import Navbar from '@/Components/LandingPage/Navbar';
 import Hero from '@/Components/LandingPage/Hero';
 import { PageProps, Plan } from '@/types';
+import { Head } from '@inertiajs/react';
 
 export default function LandingPage({
   plans,
@@ -12,6 +13,7 @@ export default function LandingPage({
 }>) {
   return (
     <div className="bg-white">
+      <Head title="Main" />
       <header className="absolute inset-x-0 top-0 z-50">
         <Navbar isAuthenticated={isAuthenticated} />
       </header>
