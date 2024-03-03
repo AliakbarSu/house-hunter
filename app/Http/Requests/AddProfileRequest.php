30a669379181f72dcc3425b87d1a228a90d177cc
Ali Sultani <aliakbar.su@hotmail.com>
2024-03-04 12:12:26 +1300

    Update validation rules and messages

--- app/Http/Requests/AddProfileRequest.php
+++ app/Http/Requests/AddProfileRequest.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\Mobile;
+use App\Rules\Name;
+use App\Rules\PhoneNumber;
 use Illuminate\Foundation\Http\FormRequest;
 
 class AddProfileRequest extends FormRequest
@@ -14,29 +17,41 @@ class AddProfileRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['nullable', 'string', 'max:15', 'min:1'],
-            'phone' => ['nullable', 'string', 'max:15', 'min:7'],
-            'mobile' => ['nullable', 'string', 'max:15', 'min:8'],
+            'name' => ['nullable', new Name()],
+            'phone' => ['nullable', new PhoneNumber()],
+            'mobile' => ['nullable', new Mobile()],
             'email' => ['nullable', 'email'],
-            'current_address.address' => ['nullable', 'string', 'max:30', 'min:5'],
+            'current_address.address' => ['nullable', 'string', 'max:100', 'min:5'],
             'current_address.move_in_at' => ['nullable', 'date_format:Y-m-d'],
             'current_address.move_out_at' => ['nullable', 'date_format:Y-m-d'],
-            'previous_address.address' => ['nullable', 'string', 'max:30', 'min:5'],
+            'previous_address.address' => ['nullable', 'string', 'max:100', 'min:5'],
             'previous_address.move_in_at' => ['nullable', 'date_format:Y-m-d'],
             'previous_address.move_out_at' => ['nullable', 'date_format:Y-m-d'],
-            'previous_address.landlord_name' => ['nullable', 'string', 'max:15', 'min:1'],
-            'previous_address.landlord_phone' => ['nullable', 'string', 'max:15', 'min:7'],
-            'previous_address.landlord_mobile' => ['nullable', 'string', 'max:15', 'min:8'],
+            'previous_address.landlord_name' => ['nullable', new Name()],
+            'previous_address.landlord_phone' => ['nullable', new PhoneNumber()],
+            'previous_address.landlord_mobile' => ['nullable', new Mobile()],
             'previous_address.landlord_email' => ['nullable', 'email'],
             'previous_address.landlord_type' => ['nullable', "in:landlord,agent,property_manager", 'string'],
             'previous_address.rent' => ['nullable', 'numeric', 'max:999999', 'min:10'],
             'previous_address.rent_frequency' => ['nullable', "in:weekly,fortnightly,monthly"],
             'references' => ['array'],
             'references.*.id' => ['nullable', 'numeric'],
-            'references.*.name' => ['nullable', 'max:15', 'min:1'],
+            'references.*.name' => ['nullable', new Name()],
             'references.*.relationship' => ['nullable', 'string', 'max:20'],
-            'references.*.phone' => ['nullable', 'string', 'max:15', 'min:7'],
-            'references.*.mobile' => ['nullable', 'string', 'max:15', 'min:8'],
+            'references.*.phone' => ['nullable', new PhoneNumber()],
+            'references.*.mobile' => ['nullable', new Mobile()],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'address.min' => 'The current address must be more than 5 characters',
+            'current_address.address.max' => 'The current address must be less than 100 characters',
+            'previous_address.address.min' => 'The previous address must be more than 5 characters',
+            'previous_address.address.max' => 'The previous address must be less than 100 characters',
+            'references.*.id.numeric' => 'The reference id must be a number',
+            'references.*.relationship.max' => 'The reference relationship must be less than 20 characters',
         ];
     }
 
