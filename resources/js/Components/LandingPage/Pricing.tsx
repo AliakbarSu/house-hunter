321e37a18f50e3f6b4977a191a382b3cc62ad6dc
Ali Sultani <aliakbar.su@hotmail.com>
2024-03-05 14:15:57 +1300

    Update pricing component text

--- resources/js/Components/LandingPage/Pricing.tsx
+++ resources/js/Components/LandingPage/Pricing.tsx
@@ -45,7 +45,7 @@ export default forwardRef(function Plans(
           Pricing
         </h2>
         <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Begin your house hunting journey for free
+          Begin your house hunting journey today
         </p>
       </div>
       <div className="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2">
